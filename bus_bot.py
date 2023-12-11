0687e562d5f8a7391b4da4ed1726867add8ec441
Stephanie Chua <78590013+cotldus@users.noreply.github.com>
2023-12-11 20:54:49 +0800

    Add following but time
    It still doesn't calculate properly though

--- bus_bot.py
+++ bus_bot.py
@@ -8,18 +8,18 @@ TOKEN = 'YOUR_BOT_TOKEN'
 
 # Bus schedules for ASR and Outram MRT
 asr_schedule = [
-    "8:00 AM", "8:15 AM", "8:30 AM", "8:45 AM", "9:00 AM", "9:15 AM", "9:30 AM", "9:45 AM",
-    "10:00 AM", "10:15 AM", "10:35 AM", "10:50 AM", "11:05 AM", "11:20 AM", "11:35 AM", "11:50 AM",
-    "12:05 PM", "12:20 PM", "12:35 PM", "12:50 PM", "1:05 PM", "1:20 PM", "2:20 PM", "2:35 PM",
-    "2:50 PM", "3:05 PM", "3:20 PM", "3:35 PM", "3:50 PM", "4:05 PM", "4:20 PM", "4:35 PM",
-    "4:50 PM", "5:05 PM", "5:40 PM", "5:55 PM", "6:10 PM", "6:25 PM", "6:40 PM", "6:55 PM",
-    "7:10 PM", "7:25 PM", "7:40 PM", "7:55 PM"
+    "08:00", "08:15", "08:30", "08:45", "09:00", "09:15", "09:30", "09:45",
+    "10:00", "10:15", "10:35", "10:50", "11:05", "11:20", "11:35", "11:50",
+    "12:05", "12:20", "12:35", "12:50", "13:05", "13:20", "14:20", "14:35",
+    "14:50", "15:05", "15:20", "15:35", "15:50", "16:05", "16:20", "16:35",
+    "16:50", "17:05", "17:40", "17:55", "18:10", "18:25", "18:40", "18:55",
+    "19:10", "19:25", "19:40", "19:55"
 ]
 
 outram_schedule = [
-    "8:04 AM", "8:34 AM", "9:04 AM", "9:34 AM", "10:04 AM", "10:39 AM", "11:09 AM", "11:39 AM",
-    "12:09 PM", "12:39 PM", "1:09 PM", "2:24 PM", "2:54 PM", "3:24 PM", "3:54 PM", "4:24 PM",
-    "4:54 PM", "5:24 PM", "5:54 PM", "6:24 PM", "6:54 PM", "7:24 PM", "7:54 PM"
+    "08:04", "08:34", "09:04", "09:34", "10:04", "10:39", "11:09", "11:39",
+    "12:09", "12:39", "13:09", "14:24", "14:54", "15:24", "15:54", "16:24",
+    "16:54", "17:24", "17:54", "18:24", "18:54", "19:24", "19:54"
 ]
 
 # Function to display disclaimer when a new user joins
@@ -57,13 +57,26 @@ def next_bus_time(update: Update, context: CallbackContext) -> None:
         return
 
     # Find the next bus time
-    for bus_time in schedule:
-        bus_time_obj = datetime.datetime.strptime(bus_time, "%I:%M %p").time()
+    for bus_time_str in schedule:
+        bus_time_obj = datetime.datetime.strptime(bus_time_str, "%H:%M").time()
         if current_time < bus_time_obj:
             time_until_next_bus = (datetime.datetime.combine(datetime.date.today(), bus_time_obj) -
                                    datetime.datetime.combine(datetime.date.today(), current_time)).seconds // 60
             update.message.reply_text(f"The next {user_location} bus will arrive in {time_until_next_bus} minutes.")
+
+            # Find the time for the bus following the next bus
+            index_of_next_bus = schedule.index(bus_time_str)
+            if index_of_next_bus < len(schedule) - 1:
+                following_bus_time = datetime.datetime.strptime(schedule[index_of_next_bus + 1], "%H:%M").time()
+                time_until_following_bus = (datetime.datetime.combine(datetime.date.today(), following_bus_time) -
+                                            datetime.datetime.combine(datetime.date.today(), current_time)).seconds // 60
+                update.message.reply_text(
+                    f"The following {user_location} bus will arrive in {time_until_following_bus} minutes."
+                )
+            else:
+                update.message.reply_text(f"There is no following {user_location} bus for today.")
             return
+
 
     # If all buses have passed for the day
     update.message.reply_text(f"All {user_location} buses for today have already passed.")
@@ -86,3 +99,4 @@ def main() -> None:
 
 if __name__ == '__main__':
     main()
+
